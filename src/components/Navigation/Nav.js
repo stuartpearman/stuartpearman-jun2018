6aa5a15f001ea8e2d696a08f30ff2b27282099d5
Stuart Pearman <stuart@element84.com>
2018-06-26 17:23:40 -0400

    nav styles and interaction

--- src/components/Navigation/Nav.js
+++ src/components/Navigation/Nav.js
@@ -3,27 +3,25 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import { connect } from 'react-redux'
 
-import { closeNav, toggleNav } from '../../state/actions/navigation'
+import { closeNav } from '../../state/actions/navigation'
 
 import NavItem from './NavItem'
 
-const Navigation = ({ toggleNav, closeNav, isOpen }) => (
-  <div className="nav-wrapper">
-    <button className="nav-toggle" onClick={toggleNav}>Toggle Nav</button>
-    <nav className="nav" data-is-open={isOpen}>
-      <button className="nav-exit" onClick={closeNav}>Close Nav</button>
-      <ul className="nav-items">
-        <NavItem dest="/styleguide/">Styleguide</NavItem>
-        <NavItem dest="/count/">Redux Example</NavItem>
-        <NavItem dest="/blog/">Blog</NavItem>
-      </ul>
-    </nav>
-  </div>
+const Navigation = ({ closeNav, isOpen }) => (
+  <nav className="nav" data-is-open={isOpen}>
+    <div className="nav-links">
+      <button className="nav-exit" onClick={closeNav}>Back</button>
+    </div>
+    <ul className="nav-items">
+      <NavItem dest="/styleguide/">Styleguide</NavItem>
+      <NavItem dest="/count/">Redux Example</NavItem>
+      <NavItem dest="/blog/">Blog</NavItem>
+    </ul>
+  </nav>
 )
 
 const mapStateToProps = ({ navigation }) => ({ isOpen: navigation.isOpen })
 const mapDispatchToProps = dispatch => ({
-  toggleNav: () => dispatch(toggleNav()),
   closeNav: () => dispatch(closeNav()),
 })
 
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,25 +1,31 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
+import { connect } from 'react-redux'
+
+import { toggleNav } from '../state/actions/navigation'
 
 import Navigation from '../components/Navigation/Nav'
 
 import '../assets/stylesheets/__all.scss'
 
-class DefaultLayout extends React.Component {
-  render() {
-    return (
-      <div className="container">
-        <header className="site-header">
-          <Link to="/" className="site-header-brand">
-            <h2>Stuart Pearman</h2>
-          </Link>
-          <Navigation />
-        </header>
-        <main className="site-main">{this.props.children()}</main>
-      </div>
-    )
-  }
-}
+const DefaultLayout = ({ children, toggleNav }) => (
+  <div className="container">
+    <header className="site-header">
+      <Link to="/" className="site-header-brand">
+        <h2>Stuart Pearman</h2>
+      </Link>
+      <button className="nav-toggle" onClick={toggleNav}>Menu</button>
+      <Navigation />
+    </header>
+    <main className="site-main">{children()}</main>
+  </div>
+)
 
-export default DefaultLayout
+const mapDispatchToProps = dispatch => ({
+  toggleNav: () => dispatch(toggleNav())
+})
+
+export default connect(
+  null, mapDispatchToProps
+)(DefaultLayout)
